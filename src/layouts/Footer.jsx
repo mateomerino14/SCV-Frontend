9b35114b0f0a31240a8113f67a8d1daa1fb83089
MAT988 <mateomerino988@gmail.com>
2026-06-03 13:50:07 -0400

    feat: Footer principal de la aplicacion

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import { COLORS } from '../constants'
 
 const styles = {
   footer: "p-1 border-t",
-  inner: "max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-2 px-4",
+  inner: "max-w-8xl mx-auto flex flex-col md:flex-row items-center justify-between gap-2 px-4",
   title: "text-xs font-bold font-inter",
   sub: "text-xs font-inter",
   year: "text-xs font-inter",
